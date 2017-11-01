6e708556aa06d924a408a590e7cb6d61a4603023
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-01 10:49:33 -0200

    build: change npm start to run hubot

--- bin/hubot.sh
+++ bin/hubot.sh
@@ -3,6 +3,5 @@
 set -e
 
 export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
-export HUBOT_HEROKU_KEEPALIVE_URL="https://wheeljack.herokuapp.com/"
 
 exec node_modules/.bin/hubot --name "wheeljack" "$@"
