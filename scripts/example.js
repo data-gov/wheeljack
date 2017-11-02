55b2a036c8a47a1188550885f0d7805aaf0f6ffb
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-02 16:06:41 -0200

    feat: expoem o parser para o script exemplo do hubot

--- scripts/example.js
+++ scripts/example.js
@@ -1,8 +1,12 @@
-var dotenv = require('dotenv')
-dotenv.config()
+require('dotenv').config()
+var parser = require('../parser')
 
 module.exports = (robot) => {
   robot.hear(/oi/i, (res) => {
     res.send('Olar')
+  })
+  robot.hear(/wit/i, async (res) => {
+    const parsedMessage = await parser('Qual candidato foi o mais votado no RS?')
+    res.send(JSON.stringify(parsedMessage))
   })
 }
