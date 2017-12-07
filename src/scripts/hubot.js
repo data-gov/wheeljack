a2037c982dc73b8e75797239858adb80d4b2e7c0
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-12-07 12:59:43 -0200

    refactor: Extrai logica de parseamento da mensagem retornada pelo wit da logica do hubot

--- src/scripts/hubot.js
+++ src/scripts/hubot.js
@@ -1,34 +1,9 @@
-// Description:
-//   Esse script escuta qualquer mensagem e a envia para um analisador
-//   de linguagem natural que extrai a intencao para frase e gera uma
-//   resposta baseado em dados historicos sobre as eleicoes do brasil
-//
-// Dependencies:
-//   "hubot-fb": "^5.0.0",
-//   "hubot-gitter": "^1.0.0",
-//   "hubot-heroku-keepalive": "1.0.3",
-//
-// Configuration:
-//   FB_APP_ID
-//   FB_APP_SECRET
-//   FB_PAGE_ID
-//   FB_PAGE_TOKEN
-//   FB_VERIFY_TOKEN
-//   FB_WEBHOOK_BASE
-//   TOKEN
-//   ROOM
-//   TELEGRAM_TOKEN
-//
-// Author:
-//   data-gov
-
 const parser = require('../')
 const ANYTHING = /.*/i
-const { createGraphqlClient } = require('../graphql')
-const { findCandidatesByRoleAndYear } = require('../graphql/queries/congressman')
+const { answer } = require('../message')
 
 function loggerMiddleware (robot) {
-  return (context, next, done) => {
+  return (context, next, _) => {
     robot.logger.info(`${context.response.message.user.name} asked me to ${context.response.message.text}`)
     next()
   }
@@ -37,20 +12,8 @@ function loggerMiddleware (robot) {
 async function parseMessage (res) {
   const userMessage = res.match.input
   const parsedMessage = await parser(userMessage)
-
-  const post = parsedMessage.entities.post[0].value
-  const year = new Date(parsedMessage.entities.datetime[0].value).getFullYear()
-
-  const client = await createGraphqlClient()
-  const result = await client.query({
-    query: findCandidatesByRoleAndYear,
-    variables: {
-      role: post.toUpperCase(),
-      year: year
-    }
-  })
-
-  res.send(JSON.stringify(result.data.candidatesByRoleAndYear))
+  const message = await answer(parsedMessage)
+  res.send(message)
 }
 
 module.exports = robot => {
