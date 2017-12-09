8df7d20b6f1714c7c082537695efd847781649e5
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-09 17:35:35 -0200

    remove hubot warning

--- src/scripts/hubot.js
+++ src/scripts/hubot.js
@@ -1,3 +1,31 @@
+// Description:
+//   Esse script escuta qualquer mensagem e a envia para um analisador
+//   de linguagem natural que extrai a intencao para frase e gera uma
+//   resposta baseado em dados historicos sobre as eleicoes do brasil
+//
+// Dependencies:
+//   "hubot-fb": "^5.0.0",
+//   "hubot-gitter": "^1.0.0",
+//   "hubot-heroku-keepalive": "1.0.3",
+//
+// Configuration:
+//  WIT_SERVER_TOKEN
+//  GRAPHQL_API_URL
+//  FB_APP_ID
+//  FB_APP_SECRET
+//  FB_PAGE_ID
+//  FB_PAGE_TOKEN
+//  FB_VERIFY_TOKEN
+//  FB_WEBHOOK_BASE
+//  HUBOT_LOG_LEVEL
+//  TOKEN
+//  ROOM
+//  HUBOT_LOG_LEVEL
+//  HUBOT_HEROKU_KEEPALIVE_URL
+//
+// Author:
+//   data-gov
+
 const { errorMessages, random } = require('../messages')
 const { answer } = require('../')
 const ANYTHING = /.*/i
