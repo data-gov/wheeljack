3d75d2ec17ce4d5c5603c121a02325b12e1c9f81
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-12 08:53:21 -0200

    feat: normalize graphql query variables

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const {
   topVotingState
 } = require('./services/election')
 
-const extractPost = post => post[0].value.toUpperCase()
-const extractState = brState => brState[0].value.toUpperCase()
+const extractPost = post => post[0].value
+const extractState = brState => brState[0].value
 const extractYear = date => new Date(date[0].value).getFullYear()
-const extractCandidateName = president => president[0].value.toUpperCase()
+const extractCandidateName = president => president[0].value
 
 exports.answer = async (message) => {
   const parsedMessage = await plnParser.message(message, {})
