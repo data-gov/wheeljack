d62f8fde7e74cc78810c2d9ebcb6bb454d661486
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-12-13 01:25:52 -0200

    feat: Finaliza implementaçao telegram

--- src/answers/static.js
+++ src/answers/static.js
@@ -2,4 +2,4 @@ exports.random = messages => messages[Math.floor(Math.random() * messages.length
 exports.errorMessages = ['Algo de errado aconteceu', 'Tive um problema ao processar sua pergunta']
 exports.greetings = ['Ola!', 'Como vai?', 'Tudo bem?', 'Olá, em que posso ajudar?']
 exports.invalidQuestion = ['Nao entendi sua pergunta', 'Poderia perguntar outra coisa?']
-exports.farewells = ['Tchau!', 'Ate mais', 'Tudo bem?']
+exports.farewells = ['Tchau!', 'Ate mais', 'Tenha um excelente dia!', 'Até a próxima']
